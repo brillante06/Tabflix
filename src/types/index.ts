e42ab9986b1719e9aedebc2b4631b1fa0dd7493a
brillante06 <marshmello37@naver.com>
2021-02-05 00:23:58 +0900

    Add movieInfo type

---
+++ src/types/index.ts
@@ -0,0 +1,16 @@
+export interface movieInfo {
+    adult: Boolean;
+    backdrop_path: string;
+    genre_ids: Array<number>;
+    id: number;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: number;
+    poster_path: string;
+    release_date: Date;
+    title: string;
+    video: Boolean;
+    vote_average: number;
+    vote_count: number;
+}
