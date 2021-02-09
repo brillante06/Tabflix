45ec83a9752ab1d43070ea0cd2abd2290e30350a
brillante06 <marshmello37@naver.com>
2021-02-09 20:09:06 +0900

    Add useEffect on search component

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -3,9 +3,18 @@ import * as S from './styles';
 import * as C from '../../utils/constants';
 import useSWR from 'swr';
 import { fetcher } from '../../utils/request';
+import { movieInfo } from '../../types';
 const Search: React.FC = () => {
     const [searchValue, setSearchValue] = useState('');
-    useEffect(() => {}, [searchValue]);
+    const [movieList, setMovieList] = useState<Array<string>>([]);
+    useEffect(() => {
+        const { data, error } = useSWR(`${C.MOVIE_SEARCH}${searchValue}`, fetcher);
+        const searchList: Array<string> = data.results.reduce(
+            (acc: Array<string>, cur: movieInfo) => acc.push(cur.title),
+            []
+        );
+        setMovieList(movieList.concat(...searchList));
+    }, [searchValue]);
     const onChange = (e: React.FormEvent<HTMLInputElement>) => {
         setSearchValue(e.currentTarget.value);
     };
