fb6ce8a83b52f1745b1ccba0cdf743e79799331f
brillante06 <marshmello37@naver.com>
2021-02-14 15:52:16 +0900

    Modify type for detail

--- src/components/Detail/index.tsx
+++ src/components/Detail/index.tsx
@@ -1,4 +1,3 @@
 import React from 'react';
-
 const Detail: React.FC = () => <h1>hello</h1>;
 export default Detail;
--- src/types/index.ts
+++ src/types/index.ts
@@ -20,6 +20,54 @@ export interface popularResponseType {
     total_pages: number;
     total_results: number;
 }
+export interface collectionType {
+    id: number;
+    name: string;
+    poster_path: string;
+    backdrop_path: string;
+}
+export interface productionCountry {
+    iso_3166_1: string;
+    name: string;
+}
+export interface company {
+    id: number;
+    logo_path: string;
+    name: string;
+    origin_country: string;
+}
+export interface spokenLanguage {
+    english_name: string;
+    iso_639_1: string;
+    name: string;
+}
+export interface detailMovie {
+    adult: boolean;
+    backdrop_path: string;
+    belongs_to_collection: collectionType;
+    budget: number;
+    genres: Array<number>;
+    homepage: string;
+    id: number;
+    imdb_id: string;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: number;
+    poster_path: string;
+    production_companies: Array<company>;
+    production_countries: Array<productionCountry>;
+    release_date: Date;
+    revenue: number;
+    runtime: number;
+    spoken_languages: Array<spokenLanguage>;
+    status: string;
+    tagline: string;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+}
 
 export interface watchProvider {
     display_priority: number;
