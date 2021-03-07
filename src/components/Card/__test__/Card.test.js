7c5168316012ec23226ab929aedddd93cd290f0c
brillante06 <marshmello37@naver.com>
2021-03-07 12:33:15 +0900

    🐛 Fix a bug

--- src/components/Card/__test__/Card.test.js
+++ src/components/Card/__test__/Card.test.js
@@ -4,19 +4,37 @@ import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/extend-expect';
 import Card from '../index';
 import { dummyMovie, dummyImage } from '../../../dummy/movieDummy';
+import userEvent from '@testing-library/user-event';
+
+const renderWithRouter = (ui, { route = '/' } = {}) => {
+    window.history.pushState({}, 'test page', route);
+    return render(ui, { wrapper: BrowserRouter });
+};
 
 describe('<Card />', () => {
     it('matches snapshot', () => {
         const onClick = jest.fn();
         const utils = render(
-            <Card title="soul" image={dummyImage} onClick={onClick} movie={dummyMovie} />
+            <Card
+                title="soul"
+                image={dummyImage}
+                onClick={onClick}
+                movie={dummyMovie}
+                id={'508422'}
+            />
         );
         expect(utils.container).toMatchSnapshot();
     });
     it('shows the props correctly', () => {
         const onClick = jest.fn();
         const utils = render(
-            <Card title="soul" image={dummyImage} onClick={onClick} movie={dummyMovie} />
+            <Card
+                title="soul"
+                image={dummyImage}
+                onClick={onClick}
+                movie={dummyMovie}
+                id={'508422'}
+            />
         );
         utils.getByText('soul');
         const image = utils.getByAltText('soul');
