1ec69cf3c720f126428bc610b2c53ef1e3f507b9
brillante06 <marshmello37@naver.com>
2021-05-12 23:51:49 +0900

    ✅ Update test on card

--- src/components/Card/__test__/Card.test.js
+++ src/components/Card/__test__/Card.test.js
@@ -1,22 +1,29 @@
 import React from 'react';
-import { fireEvent, getByTestId, getByText, render, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/extend-expect';
 import Card from '../index';
 import { dummyMovie, dummyImage } from '../../../dummy/movieDummy';
 import 'intersection-observer';
-import { act } from 'react-dom/test-utils';
-import { withRouter } from 'react-router-dom';
+import { MemoryRouter, withRouter } from 'react-router';
+import { createMemoryHistory } from '../../../utils/constants';
 
 describe('<Card />', () => {
-    const setup = () => {
+    it('matches snapshot', () => {
         const onClick = jest.fn();
-        const observe = jest.fn();
-        const unobserve = jest.fn();
-        window.IntersectionObserver = jest.fn(() => ({
-            observe,
-            unobserve,
-        }));
+        const utils = render(
+            <Card
+                image={dummyImage}
+                title="soul"
+                onClick={onClick}
+                movie={dummyMovie}
+                id={508442}
+            />
+        );
+        expect(utils.container).toMatchSnapshot();
+    });
+    it('has rating,year and title', () => {
+        const onClick = jest.fn();
         const utils = render(
             <Card
                 title="soul"
@@ -26,51 +33,11 @@ describe('<Card />', () => {
                 id={508442}
             />
         );
-        return { utils, observe, unobserve };
-    };
-
-    // it('matches snapshot', () => {
-    //     const { utils, observe } = setup();
-    //     expect(observe).toHaveBeenCalled();
-    //     expect(utils.container).toMatchSnapshot();
-    // });
-    it('get props correctly', async () => {
-        const { utils } = setup();
         utils.getByText('2020');
-        utils.getByText('soul');
         utils.getByText('⭐8.3/10');
-        utils.getByAltText('soul');
-        expect(utils.getByTestId('movie-image')).toHaveAttribute(
-            'data-src',
-            'https://image.tmdb.org/t/p/original/kf456ZqeC45XTvo6W9pW5clYKfQ.jpg'
-        );
-    });
-    it('can render to detail component', async () => {
-        const observe = jest.fn();
-        const unobserve = jest.fn();
-        window.IntersectionObserver = jest.fn(() => ({
-            observe,
-            unobserve,
-        }));
-        const onClick = jest.fn();
-        const utils = withRouter(
-            render(
-                <Card
-                    title="soul"
-                    image={dummyImage}
-                    onClick={onClick}
-                    movie={dummyMovie}
-                    id={508442}
-                />
-            )
-        );
-        await act(async () => {
-            const clickButton = utils.getByText('2020');
-            fireEvent.click(clickButton);
-            await waitFor(() => {
-                const testID = getByTestId('running-time');
-                testID.toHaveAttribute('data-testid', '101분');
-            });
-        });
+        utils.getByText('soul');
+        const image = screen.getByRole('img');
+        expect(image).toHaveAttribute('alt', 'soul');
+        expect(image).toHaveAttribute('src', dummyImage);
     });
 });
