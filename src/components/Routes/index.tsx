47387b4e9294de632f6af37676ae3d7deecaa07d
brillante06 <marshmello37@naver.com>
2021-05-18 21:30:54 +0900

    ⚡️ React.lazy and suspense on Router

--- src/components/Routes/index.tsx
+++ src/components/Routes/index.tsx
@@ -1,21 +1,25 @@
 import { Route, Router, Switch } from 'react-router-dom';
-import React from 'react';
-import Main from '../../pages/Main';
-import Detail from '../../pages/Detail';
-import { Header, Search } from '../index';
+import React, { lazy, Suspense } from 'react';
+import { Header, Loader, Search } from '../index';
 import { browserHistory } from '../../utils/constants';
 
-const Routes: React.FC = () => (
-    <Router history={browserHistory}>
-        <main>
-            <Header />
-            <Switch>
-                <Route exact path="/" component={Main}></Route>
-                <Route exact path="/search" component={Search}></Route>
-                <Route exact path="/detail/:id" component={Detail}></Route>
-            </Switch>
-        </main>
-    </Router>
-);
+const Routes: React.FC = () => {
+    const Main = lazy(() => import('../../pages/Main'));
+    const Detail = lazy(() => import('../../pages/Detail'));
+    return (
+        <Router history={browserHistory}>
+            <main>
+                <Header />
+                <Suspense fallback={<Loader />}>
+                    <Switch>
+                        <Route exact path="/" component={Main}></Route>
+                        <Route exact path="/search" component={Search}></Route>
+                        <Route exact path="/detail/:id" component={Detail}></Route>
+                    </Switch>
+                </Suspense>
+            </main>
+        </Router>
+    );
+};
 
 export default Routes;
