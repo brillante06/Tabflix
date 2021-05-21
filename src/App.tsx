538b361299b1ccf4a69ce6222547df665fb138e7
brillante06 <marshmello37@naver.com>
2021-05-21 20:29:19 +0900

    ✨ Add recoil on root

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import Routes from './components/Routes';
 import Global from './styles/global';
+import { RecoilRoot } from 'recoil';
 import theme from './styles/theme';
 
 const App: React.FC = () => (
-    <ThemeProvider theme={theme}>
-        <Global />
-        <Routes />
-    </ThemeProvider>
+    <RecoilRoot>
+        <ThemeProvider theme={theme}>
+            <Global />
+            <Routes />
+        </ThemeProvider>
+    </RecoilRoot>
 );
 
 export default App;
