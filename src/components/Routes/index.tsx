37f09c51ee87498410b577ac68884f16c02fa88b
brillante06 <marshmello37@naver.com>
2021-05-24 15:16:50 +0900

    ✨ Create About page

--- src/components/Routes/index.tsx
+++ src/components/Routes/index.tsx
@@ -2,6 +2,7 @@ import { Route, Router, Switch } from 'react-router-dom';
 import React, { lazy, Suspense } from 'react';
 import { Header, Loader } from '../index';
 import { browserHistory } from '../../utils/constants';
+import About from '../About';
 
 const Routes: React.FC = () => {
     const Main = lazy(() => import('../../pages/Main'));
@@ -16,6 +17,7 @@ const Routes: React.FC = () => {
                         <Route exact path="/" component={Main}></Route>
                         <Route exact path="/search" component={Search}></Route>
                         <Route exact path="/detail/:id" component={Detail}></Route>
+                        <Route exact path="/about" component={About}></Route>
                     </Switch>
                 </main>
             </Suspense>
