1e8762e5316ecab6f2104f143dfb995dafd92e82
brillante06 <marshmello37@naver.com>
2021-05-31 17:00:15 +0900

    🐛 Fix error with suspense

--- src/components/Routes/index.tsx
+++ src/components/Routes/index.tsx
@@ -1,23 +1,28 @@
 import { Route, Router, Switch } from 'react-router-dom';
 import React, { Suspense } from 'react';
-import { About, Header, Loader, Search } from '../index';
+import { Header, Loader } from '../index';
 import { browserHistory } from '../../utils/constants';
-import { Detail, Main } from '../../pages';
+
 import ErrorBoundary from '../ErrorBoundary';
+
+const Main = React.lazy(() => import('../../pages/Main'));
+const Detail = React.lazy(() => import('../../pages/Detail'));
+const Search = React.lazy(() => import('../../components/Search'));
+const About = React.lazy(() => import('../../components/About'));
 
 const Routes: React.FC = () => (
     <Router history={browserHistory}>
         <main>
             <Header />
             <ErrorBoundary>
-                <Switch>
-                    <Suspense fallback={<Loader />}>
+                <Suspense fallback={<Loader />}>
+                    <Switch>
                         <Route exact path="/" component={Main}></Route>
-                    </Suspense>
-                    <Route exact path="/search" component={Search}></Route>
-                    <Route exact path="/detail/:id" component={Detail}></Route>
-                    <Route exact path="/about" component={About}></Route>
-                </Switch>
+                        <Route exact path="/search" component={Search}></Route>
+                        <Route exact path="/detail/:id" component={Detail}></Route>
+                        <Route exact path="/about" component={About}></Route>
+                    </Switch>
+                </Suspense>
             </ErrorBoundary>
         </main>
     </Router>
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,14 +1,15 @@
-import React, { lazy, useState } from 'react';
+import React, { Suspense, useState } from 'react';
 import useSWR from 'swr';
 import * as S from './styles';
 import { movieInfo } from '../../types';
 import { fetcher, requestType } from '../../utils/request';
 import * as C from '../../utils/constants';
 import noImage from '../../assets/noImage.jpg';
+import { ErrorBoundary, Loader } from '../../components';
 
-const Card = lazy(() => import('../../components/Card/index'));
-const Carousel = lazy(() => import('../../components/Carousel/index'));
-const Video = lazy(() => import('../../components/Video/index'));
+const Card = React.lazy(() => import('../../components/Card'));
+const Carousel = React.lazy(() => import('../../components/Carousel'));
+const Video = React.lazy(() => import('../../components/Video'));
 
 const Main: React.FC = () => {
     const [req, setReq] = useState<string>('popular');
@@ -29,31 +30,41 @@ const Main: React.FC = () => {
         }
     };
     return (
-        <S.Container>
-            {movies && <Video video={movies.results[randomNumber]} />}
-            <S.TextContainer onClick={clickEvent}>
-                <S.Text isChecked={req === 'playing'}>Now playing</S.Text>
-                <S.Showmore>모두보기&gt;</S.Showmore>
-            </S.TextContainer>
-            {movies && (
-                <Carousel
-                    items={movies.results.map((value: movieInfo, idx) => (
-                        <Card
-                            title={value.title}
-                            id={value.id}
-                            key={idx}
-                            image={
-                                value.poster_path
-                                    ? `${C.IMAGE_URL_W500}${value.backdrop_path}`
-                                    : noImage
-                            }
-                            movie={value}
-                            tag={true}
+        <ErrorBoundary>
+            <Suspense fallback={<Loader />}>
+                <S.Container>
+                    {movies && (
+                        <ErrorBoundary>
+                            <Suspense fallback={<Loader />}>
+                                <Video video={movies.results[randomNumber]} />
+                            </Suspense>
+                        </ErrorBoundary>
+                    )}
+                    <S.TextContainer onClick={clickEvent}>
+                        <S.Text isChecked={req === 'playing'}>Now playing</S.Text>
+                        <S.Showmore>모두보기&gt;</S.Showmore>
+                    </S.TextContainer>
+                    {movies && (
+                        <Carousel
+                            items={movies.results.map((value: movieInfo, idx) => (
+                                <Card
+                                    title={value.title}
+                                    id={value.id}
+                                    key={idx}
+                                    image={
+                                        value.poster_path
+                                            ? `${C.IMAGE_URL_W500}${value.backdrop_path}`
+                                            : noImage
+                                    }
+                                    movie={value}
+                                    tag={true}
+                                />
+                            ))}
                         />
-                    ))}
-                />
-            )}
-        </S.Container>
+                    )}
+                </S.Container>
+            </Suspense>
+        </ErrorBoundary>
     );
 };
 
