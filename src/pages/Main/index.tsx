b22117a665196fe3e35e2f0a104dae2f12d94aa9
brillante06 <marshmello37@naver.com>
2021-05-31 15:42:49 +0900

    ⚡️ Change request with useSWR

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,39 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { lazy, useState } from 'react';
 import useSWR from 'swr';
 import * as S from './styles';
-import { detailMovie, movieInfo, movieList } from '../../types';
-import Carousel from '../../components/Carousel';
-import { fetcher, requestType, requestWithVideo, videoPath } from '../../utils/request';
-import { Card, Loader } from '../../components';
+import { movieInfo } from '../../types';
+import { fetcher, requestType } from '../../utils/request';
 import * as C from '../../utils/constants';
 import noImage from '../../assets/noImage.jpg';
 
-import Video from '../../components/Video';
+const Card = lazy(() => import('../../components/Card/index'));
+const Carousel = lazy(() => import('../../components/Carousel/index'));
+const Video = lazy(() => import('../../components/Video/index'));
 
 const Main: React.FC = () => {
     const [req, setReq] = useState<string>('popular');
-    const [movies, setMovies] = useState<movieInfo[]>([]);
-    const [randomMovie, setRandomMovie] = useState<Partial<detailMovie>>({});
-    const [video, setVideo] = useState<string | null>('');
-    const [isLoading, setIsLoading] = useState<boolean>(true);
-    let currentState: string = requestType[req];
+    const currentState: string = requestType[req];
     const randomNumber = Math.floor(Math.random() * 19);
-    useEffect(() => {
-        const request = async () => {
-            setIsLoading(false);
-            currentState = requestType[req];
-            const movie: movieList = await fetcher(currentState);
-            setMovies(movie.results);
-            const random: detailMovie = await fetcher(
-                requestWithVideo(movie.results[randomNumber].id)
-            );
-            setRandomMovie(random);
-            const path = await videoPath(random);
-            setVideo(path);
-            setIsLoading(true);
-        };
-        request();
-    }, [req]);
+
+    const { data: movies } = useSWR<{ results: movieInfo[] }>(currentState, fetcher, {
+        suspense: true,
+    });
 
     const clickEvent = (event: any) => {
         if (event.target.innerHTML === 'Now playing') {
@@ -44,32 +28,32 @@ const Main: React.FC = () => {
             setReq('topRated');
         }
     };
-    return isLoading ? (
+    return (
         <S.Container>
-            <Video videoURL={video} movie={randomMovie} />
+            {movies && <Video video={movies.results[randomNumber]} />}
             <S.TextContainer onClick={clickEvent}>
                 <S.Text isChecked={req === 'playing'}>Now playing</S.Text>
                 <S.Showmore>모두보기&gt;</S.Showmore>
             </S.TextContainer>
-            <Carousel
-                items={movies.map((value: movieInfo, idx) => (
-                    <Card
-                        title={value.title}
-                        id={value.id}
-                        key={idx}
-                        image={
-                            value.poster_path
-                                ? `${C.IMAGE_URL_W500}${value.backdrop_path}`
-                                : noImage
-                        }
-                        movie={value}
-                        tag={true}
-                    />
-                ))}
-            />
+            {movies && (
+                <Carousel
+                    items={movies.results.map((value: movieInfo, idx) => (
+                        <Card
+                            title={value.title}
+                            id={value.id}
+                            key={idx}
+                            image={
+                                value.poster_path
+                                    ? `${C.IMAGE_URL_W500}${value.backdrop_path}`
+                                    : noImage
+                            }
+                            movie={value}
+                            tag={true}
+                        />
+                    ))}
+                />
+            )}
         </S.Container>
-    ) : (
-        <Loader />
     );
 };
 
